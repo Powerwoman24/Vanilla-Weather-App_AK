56d852636777bef256d6fe17a4db105ea8f6eb9f
Powerwoman24 <kugler.alyssia@gmail.com>
2022-01-22 16:33:16 +0100

    added Search-Engine

--- source/script.js
+++ source/script.js
@@ -42,7 +42,19 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "ab20237aa48c3e4dbfd5087d4c74545c";
-let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "ab20237aa48c3e4dbfd5087d4c74545c";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(displayTemperature);
+}
 
-axios.get(apiURL).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
